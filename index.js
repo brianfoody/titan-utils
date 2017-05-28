3161f98db3ca9123b0e6182059af00ca88f47015
Brian Foody <brianfoody@gmail.com>
2017-05-28 17:10:09 +0800

    auto commit

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const assert = require('assert')
 const rp = require('request-promise')
 var attr = require('dynamodb-data-types').AttributeValue
 
+
+// TODO Refactor
 const createOrUpdateDynamoItem = async(function* (record, entityName) {
   var externalId = createExternalIdFromDynamoRecord(record)
   let propString = createPropertyUpdateStringFromDynamoRecord(record)
@@ -23,13 +25,10 @@ const createOrUpdateDynamoItem = async(function* (record, entityName) {
     updateString += `graph.addVertex(T.label,"${entityName}","externalId","${externalId}")${propString}`
   }
 
-  let updateResultJson = yield callTitan(updateString)
-
-  assert.equal(access(updateResultJson, 'status.code'), 200)
-
-  return updateResultJson
+  return updateString
 })
 
+// TODO Refactor
 const updateProperty = async(function* (externalId, propertyName, propertyVal) {
   let exists = yield checkVertexExists(externalId)
 
@@ -40,30 +39,18 @@ const updateProperty = async(function* (externalId, propertyName, propertyVal) {
   
   let updateString = `g.V().has("externalId","${externalId}").property("${propertyName}","${propertyVal}")`
   
-  let updateResultJson = yield callTitan(updateString)
-
-  assert.equal(access(updateResultJson, 'status.code'), 200)
-
-  return updateResultJson
+  return updateString
 })
 
-const checkVertexExists = async(function* (externalId) {
-  let resultCheckJson = yield callTitan(`g.V().has("externalId","${externalId}")`)
-  
-  let resultCheckData = access(resultCheckJson, 'result.data')
-  
-  return resultCheckData && resultCheckData.length > 0
-})
+const checkVertexExists = function (externalId) {
+  return `g.V().has("externalId","${externalId}")`
+}
 
-const getVertexTitanId = async(function* (id) {
+const getVertexTitanId = function* (id) {
   let gremlinQuery = `g.V().has("externalId","${id}").id()`
   
-  let result = yield callTitan(gremlinQuery)
-  
-  let resultCheckData = access(result, 'result.data')
-  
-  return resultCheckData && resultCheckData.length > 0 ? resultCheckData[0] : undefined
-})
+  return gremlinQuery
+}
 
 // Some nasty escaping issues so I'm just removing quotes and will replace when rendered.
 const escapeStr = (str) => {
@@ -131,86 +118,26 @@ const createExternalIdFromDynamoRecord = (record) => {
   return keyString.substring(0, keyString.length - 1)
 }
 
-const checkEdgeExists = async(function* (externalId1, externalId2, edgeLabel) {
-  let gremlinQuery = `g.V().has("externalId","${externalId1}").out("${edgeLabel}").`
-  gremlinQuery    += `has("externalId","${externalId2}")`
 
-  let resultCheckJson = yield callTitan(gremlinQuery)
-  
-  let resultCheckData = access(resultCheckJson, 'result.data')
-  
-  return resultCheckData && resultCheckData.length > 0
-})
-
-const createEdgeBetween = async(function* (externalId1, externalId2, edgeLabel) {
+const addEdgeBetween = function (externalId1, externalId2, edgeLabel, props) {
   let gremlinQuery = `g.V().has("externalId","${externalId1}").next().`
   gremlinQuery    += `addEdge("${edgeLabel}",g.V().has("externalId","${externalId2}").next(),`
   gremlinQuery    += `"on",new Date())`
   
-  let resultJson = yield callTitan(gremlinQuery)
-
-  assert.equal(access(resultJson, 'status.code'), 200)
-
-  return resultJson
-})
-
-const updateEdgeBetween = async(function* (externalId1, externalId2, edgeLabel, propString) {
-
-  // we want to get the two entities and find the exact edge between them then update the properties
-  let gremlinQuery = `g.V().has("externalId","${externalId1}").outE("${edgeLabel}").as("myEdge").`
-  gremlinQuery    += `inV().has("externalId","${externalId2}").`
-  gremlinQuery    += `select("myEdge")${propertyString}`
-  
-  let resultJson = yield callTitan(gremlinQuery)
-
-  assert.equal(access(resultJson, 'status.code'), 200)
-
-  return resultJson
-})
-
-const addEdgeBetween = async(function* (externalId1, externalId2, edgeLabel, props) {
-
-  let exists = yield checkEdgeExists(externalId1, externalId2, edgeLabel)
-
-  if (!exists) {
-    return createEdgeBetween(externalId1, externalId2, edgeLabel)
-  } 
-  // If we need to update props later
-  //   else {
-    // let propString = createEdgePropertyUpdateStringFromObject(props)
-  //   return updateEdgeBetween(externalId1, externalId2, edgeLabel, propString)
-  // }
-})
+  return gremlinQuery
+}
 
 /**
   Make sure edgeLabels has a comma beforehand i.e. edgeLabels = ',"edgeLabel1","edgeLabel2"'
 **/
-const removeEdgeBetween = async(function* (externalId1, externalId2, edgeLabels) {
+const removeEdgeBetween = function (externalId1, externalId2, edgeLabels) {
   var gremlinQuery = `g.V().has("externalId","${externalId1}").next().`
   gremlinQuery +=    `edges(Direction.OUT${edgeLabels}).`
   gremlinQuery +=    `findAll{it.inVertex().property("externalId").value()=="${externalId2}"}.each{it.remove()}`
   
-  let result = yield callTitan(gremlinQuery)
+  return gremlinQuery
+}
 
-  assert.equal(access(result, 'status.code'), 200)
-  
-  return result
-})
-
-const callTitan = async(function* (gremlinString) {
-  let titanUrl = process.env.titan_url 
-  let titanPort = process.env.titan_port
-  
-  let requestUrl = `http://${titanUrl}:${titanPort}/?gremlin=${gremlinString}`
-
-  let result = yield rp(requestUrl)
-  
-  let resultJson = JSON.parse(result)
-
-  console.log(`Titan Request: ${requestUrl}. \n Titan Response: ${util.inspect(resultJson, {depth: 2})}`)
-
-  return resultJson
-})
 
 // Titan embeds props as ID'd objects. Just need to pluck out the actual data.
 const convertTitanArrayToJson = function(titanArray) {
@@ -227,7 +154,6 @@ const convertTitanArrayToJson = function(titanArray) {
 module.exports = {
   escapeStr: escapeStr,
   unescapeStr: unescapeStr,
-  callTitan: callTitan,
   addEdgeBetween: addEdgeBetween,
   removeEdgeBetween: removeEdgeBetween,
   convertTitanArrayToJson: convertTitanArrayToJson,
